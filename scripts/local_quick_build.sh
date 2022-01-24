b46fd1201ac707cd0b57173a8898e39a4d604114
James D Bloom <733179+jamesdbloom@users.noreply.github.com>
2022-01-24 00:13:53 +0000

    removing invalid back ticks when setting JAVA_HOME

--- scripts/local_quick_build.sh
+++ scripts/local_quick_build.sh
@@ -6,7 +6,7 @@ export MAVEN_OPTS="$MAVEN_OPTS -XX:MaxPermSize=1024m -Xmx2048m -XX:+TieredCompil
 export JAVA_OPTS="$JAVA_OPTS -XX:MaxPermSize=1024m -Xmx2048m"
 # line below no longer appears to be working so using less resilient hard coded location
 #export JAVA_HOME=`/usr/libexec/java_home -v 1.8`
-export JAVA_HOME=`/Library/Java/JavaVirtualMachines/jdk1.8.0_201.jdk/Contents/Home`
+export JAVA_HOME=/Library/Java/JavaVirtualMachines/jdk1.8.0_201.jdk/Contents/Home
 echo
 java -version
 echo
