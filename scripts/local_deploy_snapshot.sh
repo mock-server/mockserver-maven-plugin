32db9db91e48f5030d2a5ca1e7e7a467d8da09bb
James D Bloom <733179+jamesdbloom@users.noreply.github.com>
2022-02-11 23:29:32 +0000

    fixed java version in local_deploy_snapshot.sh

--- scripts/local_deploy_snapshot.sh
+++ scripts/local_deploy_snapshot.sh
@@ -2,9 +2,11 @@
 
 set -e
 
-export MAVEN_OPTS="$MAVEN_OPTS -XX:MaxPermSize=1024m -Xmx2048m"
+export MAVEN_OPTS="$MAVEN_OPTS -XX:MaxPermSize=1024m -Xmx2048m -XX:+TieredCompilation -XX:TieredStopAtLevel=1"
 export JAVA_OPTS="$JAVA_OPTS -XX:MaxPermSize=1024m -Xmx2048m"
-export JAVA_HOME=`/usr/libexec/java_home -v 1.8`
+# line below no longer appears to be working so using less resilient hard coded location
+#export JAVA_HOME=`/usr/libexec/java_home -v 1.8`
+export JAVA_HOME=/Library/Java/JavaVirtualMachines/jdk1.8.0_201.jdk/Contents/Home
 echo
 java -version
 echo
