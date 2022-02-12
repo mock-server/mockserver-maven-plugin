904b9136dbd49bb52317c8ebe6c2a135516a070f
James D Bloom <733179+jamesdbloom@users.noreply.github.com>
2022-02-12 01:45:51 +0000

    fixed local_release.sh java version

--- scripts/local_release.sh
+++ scripts/local_release.sh
@@ -4,8 +4,9 @@ set -e
 
 export MAVEN_OPTS="$MAVEN_OPTS -XX:MaxPermSize=1024m -Xmx2048m"
 export JAVA_OPTS="$JAVA_OPTS -XX:MaxPermSize=1024m -Xmx2048m"
-# -agentpath:/Applications/jprofiler8/bin/macos/libjprofilerti.jnilib=port=25000
-export JAVA_HOME=`/usr/libexec/java_home -v 1.8`
+# line below no longer appears to be working so using less resilient hard coded location
+#export JAVA_HOME=`/usr/libexec/java_home -v 1.8`
+export JAVA_HOME=/Library/Java/JavaVirtualMachines/jdk1.8.0_201.jdk/Contents/Home
 echo
 java -version
 echo
